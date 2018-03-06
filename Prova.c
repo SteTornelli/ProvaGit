479cdf355c45080944e3e1125ffc5745cc3a248c
SteTornelli <tornellistefano97@gmail.com>
2018-03-06 22:35:53 +0100

    Aggiunto commento

--- Prova.c
+++ Prova.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+//defina MAX 30
 
 void main() {
+
   printf("\n>> CIAO!\n\nQuesto è un file per la prova di git  \n");
   printf("\n>> Questa è un'altra frase a caso\n");
 
