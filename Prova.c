0bda18d8585dde4eadd9804609f203514b6b7e8b
SteTornelli <tornellistefano97@gmail.com>
2018-03-06 22:22:15 +0100

    Quinto commit

--- Prova.c
+++ Prova.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void main() {
-  printf("\n>> CIAO! \n");
+  printf("\n>> CIAO!\n\nQuesto è un file per la prova di git  \n");
+  printf("\n>> Questa è un'altra frase a caso\n");
 
 }
